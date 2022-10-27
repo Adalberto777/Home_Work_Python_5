ec7e1c244c91ff1fbf4ffd075b4b8540bdf022e5
Adalberto777 <albert.tliashev@gmail.com>
2022-10-27 23:13:45 +0500

    task_3

--- task_3.py
+++ task_3.py
@@ -14,10 +14,10 @@ def read_source_text(filename: str) -> list:
         text_for_work = list(data.read())       
         return text_for_work
 
-def write_to_file(result_str: str, result2_str: str, file_name: str) -> None:
-    with open(file_name, mode="w", encoding="utf-8") as file:
-        file.write(f'Zipped text is: {result_str}' +'\n')
-        file.write(f'Unipped text is: {result2_str}' +'\n')
+def write_to_file(result_str: str, file_name: str) -> None:
+    with open(file_name, mode="a", encoding="utf-8") as file:
+        file.write(f'{result_str}'+'\n')   
+
 
 def zip_file(source_lst: list) -> str:
     counter = 1
@@ -42,14 +42,17 @@ def unzip_file(zipText: list) -> str:
            unzip_text = unzip_text + (zipText[i + 1] * int(zipText[i]))
     return unzip_text
  
+my_str = 'ABCABCABCDDDFFFFFF'
+write_to_file(my_str, "source_text_task_3.txt")
 
 sourse_text = read_source_text('source_text_task_3.txt')
 zip_text=zip_file(sourse_text)
 unzip_text = unzip_file(zip_text)
 
-
 # print(f"Zipped text is: {zip_text}")
 # print(f"Unipped text is: {unzip_text}")
 
+write_to_file(zip_text , 'result_text_task_3.txt')
+write_to_file(unzip_text, 'result_text_task_3.txt')
 
-write_to_file(zip_text , unzip_text, 'result_text_task_3.txt')
+
